4876f8e586d023efe46c56bf543e306ff0d29cdb
Tomas Vazquez <vazquezt2018@gmail.com>
2023-01-27 18:10:45 -0300

    add: react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Login from './Login'
 
 function App() {
   return (
     <div className="App">
       <h1>Cancheros</h1>
+      <p>Cancheros es la aplicación que todo futbolista necesita. Querés agendar todos los partidos a los que vas?</p>
+      
+      <Login/>
     </div>
   )
 }
---
+++ client/src/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div><button>Logear</button></div>
+  )
+}
+
+export default Login
