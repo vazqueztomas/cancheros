afb6877c2632a7790577ff027573f0661324fc3c
Tomas Vazquez <vazquezt2018@gmail.com>
2023-01-28 12:46:37 -0300

    add: credentials and cors config

---
+++ api/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  '127.0.0.1:5173'
+]
+
+module.exports = allowedOrigins;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const connectDB = require('./db');
 const server = express();
+const credentials = require('./middleware/credentials')
+const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
+server.use(credentials);
+server.use(cors({
+  origin: [
+    'http://localhost:8080',
+    'http://localhost:3000',
+    '127.0.0.1:5173'
+  ]
+}))
 server.use(express.json());
 server.get('/', (req, res, next) => {
   return res.json('Cancheros API.')
