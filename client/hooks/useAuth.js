9947238ff1a5d7d22825f2d8d1f6924cd1b6724c
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-02 17:27:54 -0300

    new useAuth

---
+++ client/hooks/useAuth.js
@@ -0,0 +1,10 @@
+import { useContext, useDebugValue } from "react";
+import AuthContext from "../context/AuthProvider";
+
+const useAuth = () => {
+  const {auth} = useContext(AuthContext);
+  useDebugValue(auth, auth => auth?.user ? 'logged in' : 'logged out');
+  return useContext(AuthContext);
+}
+
+export default useAuth;
