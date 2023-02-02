8d9242a39fd5e9885c6e2ee0341c8f322b214e91
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-02 16:09:24 -0300

    new: now you can charge the date and the other team

--- client/src/MainScreen.jsx
+++ client/src/MainScreen.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { Field, Form, Formik } from 'formik';
+
+const inputDate = (props) => (
+  <input type = 'date' {...props}/>
+)
 
 const MainScreen = () => {
+  const [visibleForm, setVisibleForm] = useState(false);
+  const initialValues = {
+    playVersus: '',
+    matchDay: '',
+  }
   return (
     <Container>
-      <button>Añadir partido</button>
+      <button onClick = {() => setVisibleForm(!visibleForm)}>Añadir partido</button>
+
+      {visibleForm ? 
+      <Formik
+        initialValues={initialValues}
+        onSubmit = {(values) => {console.log(values)}}
+      >
+        {() => (
+          <Form style={{display: 'flex', flexDirection: 'column'}}>
+          <label>Contra quién jugó tu equipo?</label>
+          <Field as = 'select' name = 'playVersus'>
+            <option>Barracas</option>
+            <option>Atletico</option>
+            <option>River</option>
+          </Field>
+
+          <label htmlFor='matchDay'>En que fecha?</label>
+          <Field as = {inputDate} name = 'matchDay'/>
+          {/* <input type = 'date' name = 'matchDay'/> */}
+
+          <button type = 'submit'>Cargar partido</button>
+        </Form>
+        )}
+      </Formik> : null}
     </Container>
   )
 }
