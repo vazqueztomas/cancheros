250886020428813fcd53ce4b9853e3b2229ede8a
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-02 18:43:05 -0300

    now added protected routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import { StateProvider } from '../config/state'
 import { setupServices } from '../services/services'
 import App from './App'
 import './index.css'
 import Login from './Login'
 import MainScreen from './MainScreen'
+import RequireAuth from './RequireAuth'
 import SelectTeam from './SelectTeam'
 import Signup from './Signup'
 import Successfull from './Successfull'
 
-const router = createBrowserRouter([
-  {path: '/', element: <App/>},
-  {path: '/login', element: <Login/>},
-  {path: '/signup', element: <Signup/>},
-  {path: '/successfull', element: <Successfull/>},
-  {path: '/select-team', element: <SelectTeam/>},
-  {path: '/mainscreen', element: <MainScreen/>}
-])
+// const router = createBrowserRouter([
+//   {path: '/', element: <App/>},
+//   {element: <RequireAuth/>,
+//     children: {
+
+//     }},
+//   {path: '/login', element: <Login/>},
+//   {path: '/signup', element: <Signup/>},
+//   {path: '/successfull', element: <Successfull/>},
+//   {path: '/select-team', element: <SelectTeam/>},
+//   {path: '/mainscreen', element: <MainScreen/>}
+// ])
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <>
+  <Route path = '/' element = {<App/>}/>
+  <Route path = '/login' element = {<Login/>}/>
+  <Route path = '/signup' element = {<Signup/>}/>
+  <Route path = '/successfull' element = {<Successfull/>}/>
+  <Route element = {<RequireAuth/>}>
+    <Route path = '/select-team' element = {<SelectTeam/>} />
+    <Route path = '/mainscreen' element = {<MainScreen/>} />
+  </Route>
+  </>
+))
 
 setupServices();
 
