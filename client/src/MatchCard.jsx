bf682c4b93643de491ae6b552e67f27476e20b25
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-04 13:13:33 -0300

    new styles for matches

--- client/src/MatchCard.jsx
+++ client/src/MatchCard.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 
 const MatchCard = ({clubOne, clubTwo, date}) => {
+  const dateParsed = date.toString().slice(0,10)
+  console.log(dateParsed)
   return (
     <Container>
-      <h3>{clubOne}</h3>
+      <div style = {{display: 'flex', flexDirection: 'column'}}>
+      <h6>{clubOne}</h6>
       <p>vs</p>
-      <h3>{clubTwo}</h3>
-      <p>El dia: {date}</p>
+      <h6>{clubTwo}</h6>
+      </div>
+      <div>
+      <p>El dia:</p>
+      <p>{dateParsed}</p>
+      </div>
     </Container>
   )
 }
@@ -16,6 +23,13 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   border: 1px solid white;
+  padding: 16px;
+  justify-content: space-around;
+  align-items: center;
+  gap: 16px;
+  & h6, p{
+    margin: 0
+  }
 `
 
 export default MatchCard
