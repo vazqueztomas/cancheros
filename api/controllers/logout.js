7cd9025cc2d6473b391437f39c3e135c2ca8fa73
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-05 23:15:25 -0300

    new logout

---
+++ api/controllers/logout.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+
+const handleLogout = async (req, res) => { 
+  const cookies = req.cookies;
+  if (!cookies.jwt) return res.sendStatus(204) // no content
+  const refreshToken = cookies.jwt;
+
+  const foundUser = await User.findOne({refreshToken}).exec();
+  if (!foundUser) {
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    return res.sendStatus(204);
+  }
+
+  foundUser.refreshToken = foundUser.refreshToken.filter(rt => rt !== refreshToken);
+  const result = await foundUser.save();
+  console.log(result);
+
+  res.clearCookie('jwt', {httpOnly: true, sameSite: 'None', secure: true});
+  res.sendStatus(204);
+}
+
+module.exports = {handleLogout};
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/auth');
 router.post('/login', authController.handleLogin);
 router.post('/signup', authController.handleSignup);
 router.get('/refresh', authController.handleRefreshToken);
+router.get('/logout', authController.handleLogout);
 
 module.exports = router;
