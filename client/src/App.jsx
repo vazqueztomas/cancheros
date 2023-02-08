4ff3d815c95968293eb60abc8893c189cbdf17b9
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-08 12:00:24 -0300

    new: background in main screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
 import { useNavigate } from 'react-router-dom'
 import { Container } from './components/styles';
 import './index.css';
+import styled from 'styled-components';
 
 function App() {
   const navigate = useNavigate();
   return (
     <Container>
-      <div>
-      <h1>Cancheros</h1>
-      <p>Tu historial de partidos. </p>
+      <div style = {{paddingBottom: '36px'}}>
+      <h1 style = {{margin: '0'}}>Cancheros</h1>
+      <p>Tu historial de partidos.</p>
       <div style = {{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
       <button onClick = {() => navigate('/login')}>INGRESAR</button>
-      <button onClick = {() => navigate('/signup')}>Registrarse</button>
+      <SecondButton onClick = {() => navigate('/signup')}>Registrarse</SecondButton>
       </div>
       </div>
     </Container>
   )
 }
+
+const SecondButton = styled.button`
+  color: var(--blue-color);
+  background: none;
+`
 
 export default App
