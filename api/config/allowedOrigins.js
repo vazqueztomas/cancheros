a0e349e52725c1304e7b6f87716a6841b28f82ad
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-13 12:24:38 -0300

    new api

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:3000',
   '127.0.0.1:5173',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://cancheros-15f93.web.app'
 ]
 
 module.exports = allowedOrigins;
