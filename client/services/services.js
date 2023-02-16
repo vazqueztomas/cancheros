6b1a23036326ae97bdf3fdf6c211dcaed90df503
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-16 16:33:33 -0300

    switched render to railwayapp

--- client/services/services.js
+++ client/services/services.js
@@ -7,7 +7,7 @@ const OPTIONS = {
 
 export function setupServices() {
   const devUriBase = "http://localhost:8080";
-  const prodUriBase = "https://cancheros.onrender.com";
+  const prodUriBase = "cancheros-production.up.railway.app";
   let getBaseUri = () =>
     window.location.host.includes("localhost") ||
     window.location.host.includes("127.0.0.1")
