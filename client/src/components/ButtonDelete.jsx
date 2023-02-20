7fca5528beda5e75ab5bfaa88be4b07139843842
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-20 17:50:25 -0300

    styles button delete

--- client/src/components/ButtonDelete.jsx
+++ client/src/components/ButtonDelete.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import styled from "styled-components";
 const ButtonDelete = () => {
-  return <div>ButtonDelete</div>;
+  return <button></button>;
 };
+
+const Button = styled.button`
+  background: red;
+  border: none;
+`;
 
 export default ButtonDelete;
