699bc8a01ca6024bca6250ecc227da93f24af2e4
Tomas Vazquez <vazquezt2018@gmail.com>
2023-02-20 17:51:16 -0300

    styles button delete

--- client/src/components/ButtonDelete.jsx
+++ client/src/components/ButtonDelete.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-const ButtonDelete = () => {
-  return <button></button>;
+const ButtonDelete = (props) => {
+  return <button onClick={props.onClick}>Borrar partido</button>;
 };
 
 const Button = styled.button`
   background: red;
   border: none;
+  padding: 8px;
+  color: white;
 `;
 
 export default ButtonDelete;
